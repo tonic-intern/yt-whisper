bfdef74edd9b78c7abd91588cd5cbb8678b69c0a
tonic-intern <118404583+tonic-intern@users.noreply.github.com>
2023-08-01 15:40:44 -0500

    Update setup.py
    Corrected egg name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="Miguel Piedrafita",
     install_requires=[
         'yt-dlp',
-        'whisper @ git+https://github.com/openai/whisper.git@main#egg=whisper'
+        'whisper @ git+https://github.com/openai/whisper.git@main#egg=openai-whisper'
     ],
     description="Generate subtitles for YouTube videos using Whisper",
     entry_points={
